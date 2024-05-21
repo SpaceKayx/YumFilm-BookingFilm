ce7a05ca75650820226938411e247812b6cb4b6f
nguyenmandat08032004@gmail.com <nguyenmandat08032004@gmail.com>
2024-05-21 15:17:21 +0700

    create PayController in package controller create folder header create file header.html in folder header create folder footer create file footer.html in folder footer update catalog >> listfilm update details >> filmdetail

---
+++ src/main/java/com/config/controller/PayController.java
@@ -0,0 +1,15 @@
+package com.config.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/booking")
+public class PayController {
+	@GetMapping()
+	public String pay()
+	{
+		return"pay";
+	}
+}
