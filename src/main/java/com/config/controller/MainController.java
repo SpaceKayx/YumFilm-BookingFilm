c9bfcf189ffd4e04162f0a9d031167fc92cc952f
nguyenmandat08032004@gmail.com <nguyenmandat08032004@gmail.com>
2024-05-23 02:05:23 +0700

    Update file pom, main create entity user, repository

--- src/main/java/com/config/controller/MainController.java
+++ src/main/java/com/config/controller/MainController.java
@@ -1,16 +1,31 @@
 package com.config.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.config.entity.User;
+import com.config.repository.UserRepository;
 
 @Controller
 @RequestMapping("/home")
 public class MainController {
 
+	@Autowired
+	UserRepository repo;
+	
 	@GetMapping()
 	public String init()
 	{
+		User user = new User();
+		
+		List<User> list = repo.findAll();
+		for (User u : list) {
+			System.out.println(u.getUsername());
+		}
 		return "index";
 	}
 	
---
+++ src/main/java/com/config/entity/User.java
@@ -0,0 +1,46 @@
+package com.config.entity;
+
+import java.io.Serializable;
+import java.security.Timestamp;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Nationalized;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The persistent class for the Users database table.
+ * 
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Users")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int Userid;
+	private LocalDateTime Birthdate;
+	@Nationalized
+	private String Email;
+	@Nationalized
+	private String Firstname;
+	@Nationalized
+	private String Lastname;
+	@Nationalized
+	private String Password;
+	@Nationalized
+	private String Phonenumber;
+	private boolean Role;
+	private boolean Sex;
+	private boolean Status;
+	@Nationalized
+	private String Username;
+
+}
